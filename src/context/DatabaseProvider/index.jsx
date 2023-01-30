531201dab601c85eabaca35dbe305140690e1eef
joaov <joaovitorcgds@gmail.com>
2023-01-30 12:23:15 -0300

    completing student registration

--- src/context/DatabaseProvider/index.jsx
+++ src/context/DatabaseProvider/index.jsx
@@ -9,6 +9,21 @@ export function DatabaseContextProvider({children}){
   const [ telephoneUnit, setTelephoneUnit ] = useState()
   const [ courseList, setCourseList ] = useState([])
   const [ currentCourse, setCurrentCourse ] = useState()
+  const [ listStudentsDB, setListStudentsDB ] = useState([])
+  const [ isLoading, setIsLoading] = useState(true)
+
+  const initializeCurrentStudent = {
+    isStudent: false,
+    created_at: "",
+    id: null,
+    id_courses: null,
+    is_paid: false ,
+    is_present: false ,
+    name: "",
+    payment_voucher: null,
+    telephone: null
+  }
+  const [ currentStudent, setCurrentStudent ] = useState(initializeCurrentStudent)
 
   async function handleGetFilteredCourseList(month, year){
     const dataUser  = getUserLocalStorage();
@@ -54,6 +69,7 @@ export function DatabaseContextProvider({children}){
       if(error){
         throw error
       }
+      setIsLoading(false)
       return setNameUser(data[0].name)
     }
   }
@@ -81,12 +97,63 @@ export function DatabaseContextProvider({children}){
     .eq('id', idParams)
 
     if(error){
+      setIsLoading(false)
       throw error;
     }
 
     console.log(data);
-    setNameUser(data[0].name)
-    setTelephoneUnit(data[0].telephone)
+    setNameUser(data[0].name);
+    setTelephoneUnit(data[0].telephone);
+    setIsLoading(!isLoading);
+  }
+
+  async function getStudentsDB(id_courses, newArray){
+    const { data, error } = await supabase
+    .from('students')
+    .select()
+    .eq("id_courses",id_courses)
+
+    if(error){
+      throw error
+    }
+    
+    setListStudentsDB(data)
+
+    const newState = newArray.map(student => {
+
+      for (let i in data){
+        if(student.id === data[i].id){
+          return {...student, 
+            name: data[i].name,
+            telephone: data[i].telephone,
+            payment_voucher: data[i].payment_voucher,
+            is_paid: data[i].is_paid,
+            is_present: data[i].is_present
+          }
+        }
+      }
+      return student;
+    })
+
+    // console.log(newState)
+    setListStudentsDB(newState)
+  }
+
+  async function getCurrentStudent(id, id_courses){
+    const  { data, error } = await supabase
+    .from("students")
+    .select("*")
+    .eq("id", id)
+    .eq("id_courses", id_courses)
+
+    if(error){
+      throw error;
+    }
+
+    if(data[0] === undefined){
+      return setCurrentStudent(initializeCurrentStudent)
+    }
+    setCurrentStudent({...data[0], isStudent: true})
   }
 
   async function getCurrentCourse(id){
@@ -128,11 +195,17 @@ export function DatabaseContextProvider({children}){
   }
 
   return (
-    <DatabaseContext.Provider value= {{ deleteCourses, cancelCourse, 
-    handleGetFilteredCourseList, handleGetName, 
-    getFilteredCourseList, getName,
-    getCurrentCourse, setCurrentCourse, setCourseList,
-    currentCourse, courseList, nameUser, telephoneUnit}}>
+    <DatabaseContext.Provider value= {{
+    handleGetFilteredCourseList, getFilteredCourseList,
+    deleteCourses, cancelCourse, 
+    getCurrentCourse, setCurrentCourse, currentCourse,
+    getStudentsDB, setListStudentsDB, listStudentsDB,
+    setCourseList,courseList,
+    getCurrentStudent, currentStudent,
+    handleGetName, getName, nameUser,
+    telephoneUnit,
+    isLoading
+    }}>
       {children}
     </DatabaseContext.Provider>
   )
